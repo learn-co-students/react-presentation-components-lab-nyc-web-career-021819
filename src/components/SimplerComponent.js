c94d91bfd5e4e3835c09a877b36d7917194d616f
adamjgordon28 <adamjgordon24@gmail.com>
2019-04-25 19:59:48 -0400

    Done.

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,17 @@
 // Code SimplerComponent Here
+import React, {Component} from 'react'
+
+function SimplerComponent(handleClick){
+
+    handleClick = () => {
+      alert("Ahhhh! You can click me?")
+    }
+  return (
+    <div onClick={this.props.handleClick}>
+    {"I am just happy"}
+    </div>
+  )
+}
+
+
+export default SimplerComponent
